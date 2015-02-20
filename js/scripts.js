e00c595972c44391a5a27a0a861071743486ab81
Mike Goren <mgoren@mortalwombat.net>
2015-02-20 11:56:57 -0800

    add ridiculous pizza images

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,11 @@ var Pizza = {
     this.topping = topping;
   },
   numSlices: function() {
-    if (this.size < 21 && this.topping == "cheese" ) { return 8; }
-    else if (this.size < 21) { return 6; }
-    else if (this.size < 36) { return 12; }
+    if (this.size <= 12 && this.topping == "cheese" ) { return 6; }
+    else if (this.size <= 12) { return 1; }
+    else if (this.size <= 16) { return 6; }
+    else if (this.size <= 24) { return 8; }
+    else if (this.size <= 36) { return 16; }
     else { return 24; }
   },
   legit: function() {
@@ -22,6 +24,23 @@ var cleanPage = function() {
   $("#error").hide();
 }
 
+var displayImg = function(slices, topping) {
+  switch(topping) {
+    case "vegetable":
+      $("#results figure").html("<img src='img/vegetable.png'>");
+      break;
+    case "cheese":
+      if(slices === 6) {
+        $("#results figure").html("<img src='img/cheese6.png'>");
+      } else {
+        $("#results figure").html("<img src='img/cheese.png'>");
+      }
+      break;
+    case "pepperoni":
+        $("#results figure").html("<img src='img/pepperoni" + slices + ".png'>");
+  }
+}
+
 
 $(document).ready(function() {
   $("#form-pizza").submit(function(event) {
@@ -32,7 +51,12 @@ $(document).ready(function() {
     pizza.init(inputtedSize, inputtedTopping);
     if (pizza.legit()) {
       var slices = pizza.numSlices();
-      $("#results h5").text("We will cut your " + pizza.size + " inch " + pizza.topping + " pizza into " + slices + " slices.");
+      if(slices === 1) {
+        $("#results h5").text("We don't cut tiny " + pizza.topping + " pizzas!");
+      } else {
+        $("#results h5").text("We cut your " + pizza.size + " inch " + pizza.topping + " pizza into " + slices + " slices.");
+      }
+      displayImg(slices, pizza.topping);
       $("#results").show();
     } else {
       $("#error h5").text("How do you expect us to slice a " + pizza.size + " inch pizza?!?");
