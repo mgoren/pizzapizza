29595b5e30d945a6d44dc3741204fbc30b2ad23d
Mike Goren <mgoren@mortalwombat.net>
2015-02-20 09:29:43 -0800

    add specs and methods for init and numSlices

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
+var Pizza = { 
+  init: function(size, topping) {
+    this.size = size;
+    this.topping = topping;
+  },
+  numSlices: function() {
+    if (this.size < 22 ) { return 8; }
+    else if (this.size < 36) { return 12; }
+    else { return 24; }
+  }
+}
+
+
+
 $(document).ready(function() {
 
 });
---
+++ spec/specs.js
@@ -0,0 +1,19 @@
+describe("Pizza", function() {
+
+  describe("init", function() {
+    it("assigns object properties size and topping", function() {
+      var myPizza = Object.create(Pizza);
+      myPizza.init(20, "cheese");
+      expect(myPizza.size).to.equal(20);
+    });
+  });
+
+  describe("numSlices", function() {
+    it("determines num of slices based on size", function() {
+      var myPizza = Object.create(Pizza);
+      myPizza.init(30, "cheese");
+      expect(myPizza.numSlices()).to.equal(12);
+    });
+  });
+
+});
